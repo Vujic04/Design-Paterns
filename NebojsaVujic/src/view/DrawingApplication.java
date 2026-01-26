fd3db52a80436c3a8955d45bec48514acfa32269
Vujic04 <vujic.nebojsa33@gmail.com>
2026-01-26 16:20:56 +0100

    Dodavanje apk-a

---
+++ NebojsaVujic/src/view/DrawingApplication.java
@@ -0,0 +1,15 @@
+package view;
+
+
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FrmDrawing frame = new FrmDrawing();
+		frame.setVisible(true);
+
+	}
+
+}
